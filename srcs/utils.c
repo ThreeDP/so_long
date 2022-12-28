8fc7c75fc01d69a400ce3a7d07214d825d2ab23b
ThreeDP <davy.paulino@aluno.ifsp.edu.br>
2022-12-28 11:21:18 -0300

    test the utils functions

---
+++ srcs/utils.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dapaulin <dapaulin@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/28 10:27:08 by dapaulin          #+#    #+#             */
+/*   Updated: 2022/12/28 10:27:08 by dapaulin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int is_path(char c)
+{
+    if (c == '0')
+        return (1);
+    return (0);
+}
+
+int is_wall(char c)
+{
+    if (c == '1')
+        return (1);
+    return (0);
+}
+
+int is_collec(char c)
+{
+    if (c == 'C')
+        return (1);
+    return (0);
+}
+
+int is_exit(char c)
+{
+    if (c == 'E')
+        return (1);
+    return (0);
+}
+
+int is_player(char c)
+{
+    if (c == 'P')
+        return (1);
+    return (0);
+}
---
+++ srcs/utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+# define UTILS_H
+
+int is_path(char c);
+int is_wall(char c);
+int is_collec(char c);
+int is_exit(char c);
+int is_player(char c);
+
+#endif
