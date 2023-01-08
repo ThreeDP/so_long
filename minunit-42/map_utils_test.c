d9eaefcb3300097fb1b30992b82a381c428900c9
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-01-08 03:43:11 -0300

    TEST: Adjust the test for the get_map

--- minunit-42/map_utils_test.c
+++ minunit-42/map_utils_test.c
@@ -1,28 +1,9 @@
 #include "../srcs/utils.h"
 #include "../srcs/so_long.h"
 #include "../srcs/get_next_line/get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
 #include "minunit.h"
-
-void    clear_map(t_map **map, void (*del)(void *))
-{
-    t_map   *head;
-    t_map   *next;
-
-    head = *map;
-    next = *map;
-    while (next)
-    {
-        head = next->next;
-        if (next->back)
-            del(next->back);
-        if (next->cols)
-            del(next->cols);
-        if (next)
-            del(next);
-        next = head;
-    }
-    *map = NULL;
-}
 
 MU_TEST_SUITE(passing_char_0_to_the_is_path_function_should_be_1)
 {
@@ -30,23 +11,26 @@ MU_TEST_SUITE(passing_char_0_to_the_is_path_function_should_be_1)
     int         i               = 0;
     t_map       *map;
     t_map       *fmap;
-    t_map       *back            = NULL;
+    t_map       *back           = NULL;
     int         fd              = open("maps_test/valid_map.ber", O_RDONLY);
     char        *exp_cols[6];
-    exp_cols[0]                 = "111111111111\n";
-    exp_cols[1]                 = "1000000C0001\n";
-    exp_cols[2]                 = "1C1110000001\n";
-    exp_cols[3]                 = "100011100C01\n";
-    exp_cols[4]                 = "100P111000E1\n";
+    size_t      amout_cols[]     = {12, 12, 12, 12, 12, 12};
+    exp_cols[0]                 = "111111111111";
+    exp_cols[1]                 = "1000000C0001";
+    exp_cols[2]                 = "1C1110000001";
+    exp_cols[3]                 = "100011100C01";
+    exp_cols[4]                 = "100P111000E1";
     exp_cols[5]                 = "111111111111";
 
     //ACT
     map = get_map(fd);
     fmap = map;
+
     //ASSERTS
     while (map)
     {
         mu_assert(back == map->back, "address is different");
+        mu_assert_int_eq(amout_cols[i], map->amout_cols);
         mu_assert_string_eq(exp_cols[i++], map->cols);
         back = map;
         map = map->next;
