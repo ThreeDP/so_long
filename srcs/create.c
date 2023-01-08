c6fca1892fa174b4b29e94be4db7e02eb8a9cd7c
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-01-08 03:49:09 -0300

    REFACTOR: relocation of functions for creating structs and creating struct info

---
+++ srcs/create.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dapaulin <dapaulin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 03:30:17 by dapaulin          #+#    #+#             */
+/*   Updated: 2023/01/08 03:30:53 by dapaulin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+#include "utils.h"
+
+t_map   *ft_mapnew(int line, char *cols, size_t amount_cols)
+{
+    t_map   *map;
+
+    map = (t_map *)malloc(sizeof(t_map));
+    if (!map)
+        return (NULL);
+    map->line = line;
+    map->cols = cols;
+    map->amount_cols = amount_cols ;
+    if (cols[map->amount_cols - 1] == '\n')
+        map->cols[map->amount_cols -= 1] = '\0';
+    map->back = NULL;
+    map->next = NULL;
+    return (map);
+}
+
+t_info  *ft_newinfo(void)
+{
+    t_info  *info;
+
+    info = (t_info *)malloc(sizeof(t_info));
+    if (!info)
+        return (NULL);
+    info->p = 0;
+    info->c = 0;
+    info->e = 0;
+    info->walk = 0;
+    return (info);  
+}
