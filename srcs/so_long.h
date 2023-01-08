9d8f73856b8b33c3f095f91a29a1ff97cdceda12
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-01-08 05:58:05 -0300

    FEAT: Add new function message error

--- srcs/so_long.h
+++ srcs/so_long.h
@@ -1,12 +1,33 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include "utils.h"
+
 # define W_WIDTH 850
 # define W_HEIGHT 408
 # define START_X 0
 # define START_Y 0
 
-# define MAPY "1111111111111111111111111\n1000000000000000000000001\n1000000000000000000000001\n1000000000000000000000001\n1000000000000000000000001\n1000000000000000000000001\n100000000000p000000000001\n1000000000000000000000001\n1000000000000000000000001\n1000000000000000000000001\n1000000000000000000000001\n1111111111111111111111111"
+# define ERRP 15
+# define ERRE 4
+# define ERRW 22
+# define ERRC 2
+# define ERRS 18
+# define ERRF 5
+
+# define ERR "Error\n"
+# define ERRPLAYER "No player in the map!"
+# define ERREXIT "No exit in the map!"
+# define ERRWALL "The wall is broken!"
+# define ERRCOLLEC "No collecs in the map!"
+# define ERRSHAPE "Map in wrong shape!"
+# define ERRPHATH "No valid path!"
 
 typedef struct  s_data {
     void	*img;
@@ -15,18 +36,36 @@ typedef struct  s_data {
     int		img_h;
 }               t_data;
 
-/*MAP UTILS*/
-typedef struct  a_map
+typedef struct s_info 
+{
+    int     p;
+    int     e;
+    int     c;
+    int     walk;
+    char    err;
+}               t_info;
+
+typedef struct a_map
 {
     int             line;
     char            *cols;
+    size_t          amount_cols;
     struct a_map    *back;
     struct a_map    *next;
 }               t_map;
 
-t_map       *ft_mapnew(int line, char *cols);
+/*create structs*/
+t_map       *ft_mapnew(int line, char *cols, size_t amount_cols);
+t_info      *ft_newinfo(void);
+
+int     map_validation(t_map *map, t_info **info);
+
+/*MAP UTILS*/
+void        clear_map(t_map **map, void (*del)(void *));
 void        ft_mapadd_back(t_map **map, t_map *new);
 t_map       *ft_maplast(t_map *map);
 t_map       *get_map(int fd);
+
+void    merr(char *err);
 
 #endif
