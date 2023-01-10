760f31e3c4b7143528105b42ed933209b3037856
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-01-10 04:12:59 -0300

    Add the path_validation and the start_game funciton in the main

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,63 +6,19 @@
 /*   By: dapaulin <dapaulin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 17:15:10 by dapaulin          #+#    #+#             */
-/*   Updated: 2023/01/08 06:48:45 by dapaulin         ###   ########.fr       */
+/*   Updated: 2023/01/10 03:43:55 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
-#include <stdio.h>
-#include <unistd.h>
 #include "./libft/srcs/libft.h"
 #include "so_long.h"
 #include "utils.h"
-/*
-void    draw_img(t_data *img, t_data *img1, void *mlx, void *mlx_win,  const char *mapy)
-{
-	int i;
-	int	x;
-	int y;
-	
-	i = 0;
-	y = 0;
-	while (mapy[i])
-	{
-		x = 0;
-		while (mapy[i] != '\n')
-		{
-			if ('p' == mapy[i])
-				mlx_put_image_to_window(mlx, mlx_win, img1->img, x, y);
-			if ('1' == mapy[i])
-				mlx_put_image_to_window(mlx, mlx_win, img->img, x, y);	
-			x += img->img_w;
-            i++;
-		}
-		i++;
-		y += img->img_h;
-        
-	}
-}
 
-void	create_img(t_data *data, void *mlx, char *path)
-{	
-    data->img_path = path;
-    data->img = mlx_xpm_file_to_image(mlx, data->img_path, &data->img_w, &data->img_h);
-}
-
-void	openwin(t_map *map, t_info *info)
-{
-	void	*mlx;
-    void	*mlx_win;
-	
-	mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, W_WIDTH, W_HEIGHT, "so_long");
-    mlx_loop(mlx);
-}
-*/
 int main(int ac, char **av)
 {
 	int		fd;
     t_map	*map;
+	t_map	*cpy;
 	t_info	*info;
 
 	if (ac != 2)
@@ -71,9 +27,19 @@ int main(int ac, char **av)
 	if (!fd)
 		return (1);
     map = get_map(fd);
+	close(fd);
+	cpy = cpy_map(map);
 	info = ft_newinfo();
 	if (!map_validation(map, &info))
 		handle_err(&map, &info);
+	if (path_validation(cpy, info))
+	{
+		clear_map(&cpy, free);
+		handle_err(&map, &info);
+	}
+	start_game(map, info);
+	clear_map(&map, free);
+	free(info);
 	return (0);
 }
 
--- srcs/so_long.h
+++ srcs/so_long.h
@@ -21,6 +21,8 @@
 # define ERRS 18
 # define ERRF 5
 
+# define PXL 40
+
 # define ERR "Error\n"
 # define ERRPLAYER "No player in the map!\n"
 # define ERREXIT "No exit in the map!\n"
@@ -30,44 +32,59 @@
 # define ERRPHATH "No valid path!\n"
 
 typedef struct  s_data {
-    void	*img;
-    char	*img_path;
-    int		img_w;
-    int		img_h;
+	void	*img;
+	char	*img_path;
+	int		img_w;
+	int		img_h;
 }               t_data;
-
-typedef struct s_info 
-{
-    int     p;
-    int     e;
-    int     c;
-    int     walk;
-    char    err;
-}               t_info;
 
 typedef struct a_map
 {
-    int             line;
-    char            *cols;
-    size_t          amount_cols;
-    struct a_map    *back;
-    struct a_map    *next;
+	int             line;
+	char            *cols;
+	size_t          amount_cols;
+	struct a_map    *back;
+	struct a_map    *next;
 }               t_map;
 
-/*create structs*/
-t_map       *ft_mapnew(int line, char *cols, size_t amount_cols);
-t_info      *ft_newinfo(void);
+typedef struct s_info 
+{
+	int     p;
+	int     e;
+	int     c;
+	int     walk;
+	char    err;
+	int		n_lines;
+}               t_info;
 
-int     map_validation(t_map *map, t_info **info);
-
-/*MAP UTILS*/
+// Map utils
+t_map       *cpy_map(t_map *map);
 void        clear_map(t_map **map, void (*del)(void *));
 void        ft_mapadd_back(t_map **map, t_map *new);
 t_map       *ft_maplast(t_map *map);
 t_map       *get_map(int fd);
 
-void    merr(char *err);
-void    handle_err(t_map **map, t_info **info);
-void    map_path(t_map **map, int x, int *c);
+// Created Structs
+t_map       *ft_mapnew(int line, char *cols, size_t amount_cols);
+t_info      *ft_newinfo(void);
+
+// Game tools
+int			find_player(t_map **map, char c);
+
+// Game windows
+void		start_game(t_map *map, t_info *info);
+
+// Validations
+int         map_validation(t_map *map, t_info **info);
+int         path_validation(t_map *map, t_info *info);
+size_t  	search_elems(char *str, t_info **info);
+void        map_path(t_map *map, int x);
+
+// Error Handle
+void        merr(char *err);
+void        handle_err(t_map **map, t_info **info);
+
+// Utils
+size_t  	all_wall(char *wall);
 
 #endif
