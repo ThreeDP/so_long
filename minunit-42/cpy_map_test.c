5d0eba70f808cc035b8f27d3da91008ec7927e5b
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-01-10 01:29:50 -0300

    FIX: Correct the cpy map and or test

--- minunit-42/cpy_map_test.c
+++ minunit-42/cpy_map_test.c
@@ -10,16 +10,20 @@ MU_TEST_SUITE(test)
 {
     //ARRANGE
     t_map       *map;
+    t_map       *fmap;
     t_map       *cpy;
+    t_map       *fcpy;
     int         fd              = open("maps_test/valid_map.ber", O_RDONLY);
 
     //ACT
     map = get_map(fd);
+    fmap = map;
     cpy = cpy_map(map);
+    fcpy = cpy;
     close(fd);
 
     //ASSERTS
-    while (map->next)
+    while (map)
     {
         mu_assert_string_eq(map->cols, cpy->cols);
         mu_assert_int_eq(map->line, cpy->line);
@@ -27,12 +31,8 @@ MU_TEST_SUITE(test)
         map = map->next;
         cpy = cpy->next;
     }
-    while (map->back)
-        map = map->back;
-    while (cpy->back)
-        cpy = cpy->back;
-    clear_map(&map, free);
-    clear_map(&cpy, free);
+    clear_map(&fmap, free);
+    clear_map(&fcpy, free);
 }
 
 MU_TEST_SUITE(test_suite)
