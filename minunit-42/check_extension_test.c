4cced440ca783c93718a92060908fd536416e24f
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-01-17 10:18:34 -0300

    TEST: Add new tests for the extension validation

--- minunit-42/check_extension_test.c
+++ minunit-42/check_extension_test.c
@@ -66,12 +66,60 @@ MU_TEST_SUITE(passing_a_empty_str_should_be_1)
     mu_assert_int_eq(expected, result);
 }
 
+MU_TEST_SUITE(passing_a_str_dot_ber_only_should_be_1)
+{
+    //ARRANGE
+    int         result;
+    char        *s1                 = ".ber";
+    char        *set                = ".ber";
+    int         expected            = 1;
+
+    //ACT
+    result = check_extension(s1, set);
+
+    //ASSERTS
+    mu_assert_int_eq(expected, result);
+}
+
+MU_TEST_SUITE(passing_a_path_a_str_dot_ber_only_should_be_1)
+{
+    //ARRANGE
+    int         result;
+    char        *s1                 = "/path/.ber";
+    char        *set                = ".ber";
+    int         expected            = 1;
+
+    //ACT
+    result = check_extension(s1, set);
+
+    //ASSERTS
+    mu_assert_int_eq(expected, result);
+}
+
+MU_TEST_SUITE(passing_a_path_a_str_map_dot_ber_should_be_0)
+{
+    //ARRANGE
+    int         result;
+    char        *s1                 = "/path/map.ber";
+    char        *set                = ".ber";
+    int         expected            = 0;
+
+    //ACT
+    result = check_extension(s1, set);
+
+    //ASSERTS
+    mu_assert_int_eq(expected, result);
+}
+
 MU_TEST_SUITE(test_suite)
 {
     MU_RUN_TEST(passing_a_str_with_dot_ber_should_be_zero);
     MU_RUN_TEST(passing_a_str_no_dot_ber_should_be_one);
     MU_RUN_TEST(passing_a_str_ber_dot_ber_should_be_zero);
     MU_RUN_TEST(passing_a_empty_str_should_be_1);
+    MU_RUN_TEST(passing_a_str_dot_ber_only_should_be_1);
+    MU_RUN_TEST(passing_a_path_a_str_dot_ber_only_should_be_1);
+    MU_RUN_TEST(passing_a_path_a_str_map_dot_ber_should_be_0);
 }
 
 int main() {
