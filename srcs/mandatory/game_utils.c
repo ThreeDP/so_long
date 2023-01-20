4c53c874ba94dde9e011a8e995c2d1428d49d483
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-01-20 05:16:31 -0300

    REFACTORY: Refactory code base and correct problems in handle errs presentations

---
+++ srcs/mandatory/game_utils.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dapaulin <dapaulin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 18:57:16 by dapaulin          #+#    #+#             */
+/*   Updated: 2023/01/20 04:51:57 by dapaulin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+#include "../libft/srcs/libft.h"
+#include "../get_next_line/get_next_line.h"
+#include "utils.h"
+
+void	check_screen_size(char **av)
+{
+	int		fd;
+	char	*str;
+	int		lines;
+	int		cols;
+
+	lines = 0;
+	cols = 0;
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+		merr(ERR, 2, ERRA);
+	while (1)
+	{
+		str = get_next_line(fd);
+		if (!str)
+			break ;
+		free(str);
+		lines++;
+		cols++;
+	}
+	close(fd);
+	if ((PXL * lines) > MAX_H || (PXL * cols) > MAX_W)
+		merr(ERR, 1, ERRO);
+	if ((PXL * lines) < MIN_H && (PXL * cols) < MIN_W)
+		merr(ERR, 1, ERRN);
+}
+
+void	clean_window(t_info *info)
+{
+	if (info->win)
+	{
+		mlx_clear_window(info->mlx, info->win);
+		mlx_destroy_window(info->mlx, info->win);
+		info->win = NULL;
+	}
+	if (info->mlx)
+	{
+		mlx_destroy_display(info->mlx);
+		free(info->mlx);
+		info->mlx = NULL;
+	}
+}
+
+int	end_game(t_info *info)
+{
+	clear_map(&info->map, free);
+	clean_all_data(info);
+	clean_window(info);
+	if (info)
+	{
+		free(info);
+		info = NULL;
+	}
+	exit(0);
+}
+
+void	start_game(t_map *map, t_info *info)
+{
+	int	n_lines;
+	int	n_cols;
+
+	info->mlx = NULL;
+	info->win = NULL;
+	n_lines = info->n_lines;
+	n_cols = map->n_cols;
+	info->map = map;
+	info->mlx = mlx_init();
+	if (!info->mlx)
+		clean_initial(info, NULL, ERRA);
+	set_elems(info);
+	info->win = mlx_new_window(info->mlx, PXL * n_cols, PXL * n_lines, NAME);
+	if (!info->win)
+		clean_initial(info, NULL, ERRA);
+	mlx_hook(info->win, 17, 0L, end_game, info);
+	mlx_key_hook(info->win, key_hook, info);
+	mlx_loop_hook(info->mlx, &put_in_window, info);
+	mlx_loop(info->mlx);
+	return ;
+}
---
+++ srcs/mandatory/memory_free.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memory_free.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dapaulin <dapaulin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 17:09:17 by dapaulin          #+#    #+#             */
+/*   Updated: 2023/01/20 03:27:01 by dapaulin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.h"
+#include "so_long.h"
+#include "../libft/srcs/libft.h"
+
+void	clean_data(t_data *data[])
+{
+	int	i;
+
+	i = 0;
+	while (i < 4)
+	{
+		if (data[i])
+		{
+			free(data[i]);
+			data[i++] = NULL;
+		}
+	}
+}
+
+void	clean_all_data(t_info *info)
+{
+	int	i;
+
+	i = 0;
+	while (i < 4)
+		clean_data(info->player[i++]);
+	clean_data(info->collec);
+	clean_data(info->exit);
+	clean_data(info->wall);
+	if (info->floor)
+	{
+		free(info->floor);
+		info->floor = NULL;
+	}
+}
+
+void    clean_initial(t_info *info, t_map *cpy, int err)
+{
+    clear_map(&cpy, free);
+	clear_map(&info->map, free);
+	clean_all_data(info);
+	clean_window(info);
+	if (info)
+	{
+		free(info);
+		info = NULL;
+	}
+    merr(ERR, err, info->err);
+}
+
+void	destroy_elems(void *mlx, t_data *data[])
+{
+	int	i;
+
+	i = 0;
+	while (i < 4)
+	{
+		if (data[i]->img)
+		{
+			mlx_destroy_image(mlx, data[i]->img);
+			data[i]->img = NULL;
+		}
+		i++;
+	}
+}
+
+void	clean_elems(void *mlx, t_info *info)
+{
+	int	i;
+
+	i = 0;
+	if (info->floor->img)
+	{
+		mlx_destroy_image(mlx, info->floor->img);
+		info->floor->img = NULL;
+	}
+	destroy_elems(info->mlx, info->exit);
+	destroy_elems(info->mlx, info->wall);
+	destroy_elems(info->mlx, info->collec);
+	while (i < 4)
+		destroy_elems(info->mlx, info->player[i++]);
+}
